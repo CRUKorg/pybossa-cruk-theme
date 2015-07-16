25c92c236bbe5b7861c496124d4f6665c583aa8c
Dryden williams <hello@drydenwilliams.co.uk>
2015-07-16 16:12:56 +0100

    Adding them scripts inline for now - easier to update

--- static/js/zoom.js
+++ static/js/zoom.js
@@ -1,31 +1,31 @@
-
- "use strict";
-
-    /*
-     * Elevate Zoom
-     *
-     */
-
-    function Zoom() {
-        Grid.call(this);
-    }
-
-    Zoom.prototype = Object.create(Grid.prototype);
-
-    Zoom.prototype.addZoom = function( element ){
-        element.elevateZoom({
-                    zoomWindowPosition: "grid-info",
-                    lensFadeIn: 500,
-                    lensFadeOut: 500,
-                    zoomWindowWidth: 455,
-                    zoomWindowHeight: 455,
-                });
-    }
-
-    Zoom.prototype.removeZoom = function( element ){
-
-        $.removeData( element , 'elevateZoom');
-        $('.zoomContainer').remove();
-        $('.zoomWindowContainer').remove();
-
-    }
+//
+// "use strict";
+//
+//    /*
+//     * Elevate Zoom
+//     *
+//     */
+//
+//    function Zoom() {
+//        Grid.call(this);
+//    }
+//
+//    Zoom.prototype = Object.create(Grid.prototype);
+//
+//    Zoom.prototype.addZoom = function( element ){
+//        element.elevateZoom({
+//                    zoomWindowPosition: "grid-info",
+//                    lensFadeIn: 500,
+//                    lensFadeOut: 500,
+//                    zoomWindowWidth: 455,
+//                    zoomWindowHeight: 455,
+//                });
+//    }
+//
+//    Zoom.prototype.removeZoom = function( element ){
+//
+//        $.removeData( element , 'elevateZoom');
+//        $('.zoomContainer').remove();
+//        $('.zoomWindowContainer').remove();
+//
+//    }
