5faec2ad4cc8f352cce1cdd9cd73cd7ce1d362e3
Dryden williams <hello@drydenwilliams.co.uk>
2015-07-16 10:09:38 +0100

    Adding grid.js

--- static/js/grid.js
+++ static/js/grid.js
@@ -1,224 +1,188 @@
-"use strict";
+  "use strict";
 
-/**
- *
- * Grid Constructor function
- *
- */
-function Grid() {
-    this.canvas;
-    this.grid = [];
-    this.grid__item_active;
-    this.task = {};
-    this.task["answer"] = [];
-    this.task["image_difficulty"] = 1;
-};
+    /**
+     * grid.js
+     * Grid Constructor function
+     * Its a bit of a miss match of vanilla JS and jQuery...sorry,
+     * wanted to do everything in JS but for speed i did jQuery
+     *
+     */
+    function Grid(task) {
 
+        //Modal.call(this);
 
-/**
- *
- * Main initializing method
- *
- */
-Grid.prototype.init = function () {
+        this.task = task;
+        this.activateCell();
 
-    this.init_zoom();
-    this.create_grid(2, 2);
-    this.activate_cell();
-
-
-    $("#grid_size").on("change", $.proxy(this.change_grid, this));
-    $("#difficulty").on("change", $.proxy(this.difficulty, this))
-    $("#refresh").on("click", $.proxy(this.change_grid, this));
-    $("#next-slide").on("click", $.proxy(this.next_slide, this));
-
-
-}
-
-
-/**
- *
- * For initializing the Zoom functionality
- *
- */
-Grid.prototype.init_zoom = function () {
-    $("#slide").elevateZoom({
-        //scrollZoom : true,
-        constrainType: "height",
-        constrainSize: 274,
-        cursor: "pointer",
-        zoomWindowPosition: 1,
-        zoomWindowOffetx: 10,
-    });
-}
-
-
-/**
- *
- * For creating a grid overlayed over image
- *
- */
-Grid.prototype.create_grid = function (rows, columns) {
-    this.canvas = Raphael("canvas", $("#slide").width(), $("#slide").height());
-    var gridHeight = $("#slide").height() / columns;
-    var gridWidth = $("#slide").width() / rows;
-
-    var x, y;
-    for (y = 0; y < rows; y += 1) {
-        for (x = 0; x < columns; x += 1) {
-
-            var offsetH = y;
-            var offsetW = x;
-            var moveDown = (y + gridHeight - offsetH) * y;
-            var moveRight = (x + gridWidth - offsetW) * x;
-
-            var grid_item = this.canvas.rect(moveRight, moveDown, gridWidth, gridHeight)
-                .attr({
-                    "stroke": "#fff"
-                }).animate()
-            grid_item.node.setAttribute("class", "grid__item");
-
-
-            grid_item["x"] = x;
-            grid_item["y"] = y;
-            grid_item["has_cancer"] = null;
-
-            this.grid.push(grid_item);
-
-        }
+        /**
+         *
+         * Initializing the elevateZoom library
+         * www.elevateweb.co.uk/image-zoom
+         *
+         */
+        $(".img-polaroid").elevateZoom({
+            zoomWindowPosition: "grid-info",
+            lensFadeIn: 500,
+            lensFadeOut: 500,
+            cursor: "pointer",
+            // Changing these two numbers changes the zoomWindowWidth
+            // Also the side of the .zoomLens (box over image)
+            zoomWindowWidth: 455,
+            zoomWindowHeight: 455,
+        });
     }
-}
+
+    //Grid.prototype = Object.create(Modal.prototype);
+
+    /**
+     *
+     * For creating a grid overlayed over the image
+     *
+     */
+    Grid.prototype.createGrid = function () {
+
+        /*
+         * Using raphael so we can use methods like isPointInside
+         * and calculate how close the click on grid are.
+         * Also nice and cross browser :D
+         *
+         * Maybe d3 might have been better!?
+         */
+        var rows = 6,
+            columns = 6,
+            canvas = Raphael("canvas", $(".img-polaroid").width(), $(".img-polaroid").height()),
+            gridHeight = $(".img-polaroid").height() / columns,
+            gridWidth = $(".img-polaroid").width() / rows;
+
+        // creates 36 grid squares in raphael
+        var x, y;
+        for (y = 0; y < rows; y += 1) {
+            for (x = 0; x < columns; x += 1) {
+
+                var offsetH = y,
+                    offsetW = x,
+                    moveDown = (y + gridHeight - offsetH) * y,
+                    moveRight = (x + gridWidth - offsetW) * x,
+                    gridSquare = canvas.rect(moveRight, moveDown, gridWidth, gridHeight).attr({
+                        "stroke": "#fff"
+                    });
+
+                gridSquare.node.setAttribute("class", "grid-item");
+
+                gridSquare.column = x;
+                gridSquare.row = y;
+                gridSquare.has_cancer = null;
+
+                // Save all grid squares in array within main task object
+                // We can then grab this when saving the info at the end
+                this.task.answer.squares.push(gridSquare);
+
+            }
+        }
+    };
 
 
-Grid.prototype.activate_cell = function () {
-    var delay = 200;
-    var clicks = 0;
-    var timer = null;
-    var _this = this;
+    /**
+     *
+     * Listen for lick on .zoomLens and find the closed square
+     *
+     */
+    Grid.prototype.activateCell = function () {
 
-    $("body").on("click", ".zoomLens", function (evt) {
+        var clicks = 0;
+        var timer = null;
+        var _this = this;
 
-        evt.preventDefault();
-        clicks++;  //count clicks
+        $("body").on("click touchstart", ".zoomLens", function (evt) {
 
-        this.grid__item_active = _this.loop();
+            evt.preventDefault();
+            clicks++; //count clicks
 
-        var grid__item_active = this.grid__item_active;
-        var grid__item_node = this.grid__item_active.node;
-        var grid__item_active_className = grid__item_node.className.animVal;
+            // Find the cloest grid square to click on the zoomLens
 
+            var activeSquare = _this.findClosestSquare();
+            var activeSquareNode = activeSquare.node;
 
-        if (clicks === 1) {
-            timer = setTimeout(function () {
+            // Not able to use jQuery .click or .dblclick for some reason i can't remember
+            // If we can then should use jQuery...might be quicker and easier
 
-                if (grid__item_active_className != "grid__item grid__item--success") {
-                    grid__item_node.setAttribute("class", "grid__item grid__item--success");
-                    grid__item_active.has_cancer = true;
-                } else {
-                    grid__item_node.setAttribute("class", "grid__item");
-                    grid__item_active.has_cancer = null;
-                }          //after action performed, reset counter
+            if (clicks === 1) {
+                timer = setTimeout(function () {
+                    // Adds and removes classes to grid squares making them red or green
+                    if (activeSquareNode.className.animVal != "grid-item grid-item-success") {
+                        activeSquareNode.setAttribute("class", "grid-item grid-item-success");
+                        activeSquare.has_cancer = true;
+                    } else {
+                        activeSquareNode.setAttribute("class", "grid-item");
+                        activeSquare.has_cancer = null;
+                    } //after action performed, reset counter
+                    clicks = 0;
+                }, 250);
+            } else {
+                clearTimeout(timer); //prevent single-click action
+                activeSquareNode.setAttribute("class", "grid-item grid-item-error");
+                activeSquare.has_cancer = false;
                 clicks = 0;
-            }, delay);
-        } else {
-            clearTimeout(timer);    //prevent single-click action
-            grid__item_node.setAttribute("class", "grid__item grid__item--error");
-            grid__item_active.has_cancer = false;
+            }
+        });
+    };
 
-            clicks = 0;
+    /**
+     *
+     * Loop through the grid items
+     * and find the closest matching grid
+     * that have the clicked coordinates
+     *
+     */
+    Grid.prototype.findClosestSquare = function () {
+
+        var _this = this,
+            activeSquare,
+            target = $(".zoomLens"),
+            target_pos = {
+                x: parseInt(target.css("left")),
+                y: parseInt(target.css("top"))
+            };
+
+        // Center the zoom cursor to middle of square
+        target_pos.x = target_pos.x + 34.85;
+        target_pos.y = target_pos.y + 34.85;
+
+        // Find the closet grid item
+        $.each(_this.task.answer.squares, function (key, value) {
+            if (value.isPointInside(target_pos.x, target_pos.y)) {
+                activeSquare = value;
+            }
+        });
+        return activeSquare;
+    };
+
+
+    Grid.prototype.selectAllSquares = function () {
+        for (var i = 0; i < this.task.answer.squares.length; i++) {
+            var gridSquare = this.task.answer.squares[i],
+                gridSquareNode = this.task.answer.squares[i].node;
+            gridSquare.has_cancer = true;
+            gridSquareNode.setAttribute("class", "grid-item grid-item-success");
         }
-    });
-}
-
-
-/**
- *
- * Loop through the grid items
- * and find the closest matching grid
- * that have the clicked coordinates
- *
- */
-Grid.prototype.loop = function () {
-
-    var _this = this;
-    var active_node;
-
-
-    var target = $(".zoomLens");
-    var target_pos = {
-        x: parseInt(target.css("left")),
-        y: parseInt(target.css("top"))
+    }
+    Grid.prototype.deselectAllSquares = function () {
+        for (var i = 0; i < this.task.answer.squares.length; i++) {
+            var gridSquare = this.task.answer.squares[i],
+                gridSquareNode = this.task.answer.squares[i].node;
+            gridSquare.has_cancer = null;
+            gridSquareNode.setAttribute("class", "grid-item");
+        }
     }
 
-    // Center the zoom cursor to middle of square
-    target_pos.x = target_pos.x
-    target_pos.y = target_pos.y
 
-    // Find the closet grid item
-    $.each(this.grid, function (key, value) {
+    /*
+     *
+     * End of Grid class
+     *
+     * */
 
-        if (value.isPointInside(target_pos.x, target_pos.y)) {
-            active_node = value;
-            _this.show_results(value);
-
-        }
-    });
-
-    return active_node;
-
-}
-
-/**
- *
- * Add click listener to each grid cell
- *
- */
-Grid.prototype.show_results = function (active_node) {
-    $("#result-row").text(active_node.y);
-    $("#result-column").text(active_node.x);
-    $("#result-item").text(active_node.id + 1);
-}
+    /************************************************************************************/
 
 
-Grid.prototype.change_grid = function () {
-    var str = "";
 
-    $("#grid_size option:selected").each(function () {
-        str = $(this).val();
-    });
-    console.log(str);
-
-
-    this.grid = [];
-    this.canvas.clear();
-    $("svg").remove();
-    this.create_grid(str, str);
-
-    $.each(this.grid, function (key, value) {
-        value.id = key;
-    });
-
-}
-
-Grid.prototype.difficulty = function () {
-    var a = "";
-    $("#difficulty option:selected").each(function () {
-        a = $(this).val();
-    });
-    console.log(a);
-    this.task["image_difficulty"] = a;
-}
-
-Grid.prototype.next_slide = function () {
-    var _this = this;
-    $.each(this.grid, function (key, value) {
-        var obj = {
-            column: value.x,
-            row: value.y,
-            answer: value["has_cancer"]
-        }
-        _this.task["answer"].push(obj);
-    });
-
-
-}
